37dad437ea90f8bcba92e790e10f06226b7214d5
Dr Nic Williams <drnicwilliams@gmail.com>
2011-11-18 21:45:49 -0800

    Fix the "I am have a local recipe" step

--- features/step_definitions/fixture_project_steps.rb
+++ features/step_definitions/fixture_project_steps.rb
@@ -9,7 +9,7 @@ end
 Given /^I am have a local recipe "([^\"]*)" at "\/tmp\/ey-recipes\/([^"]*)"$/ do |name, repeat_name|
   name.should == repeat_name
   fixture_recipe = File.join(@fixtures_path, "recipes", name)
-  FileUtils.rm_rf(File.basename(@tmp_recipes_path))
-  FileUtils.mkdir_p(File.basename(@tmp_recipes_path))
+  FileUtils.rm_rf(@tmp_recipes_path)
+  FileUtils.mkdir_p(@tmp_recipes_path)
   FileUtils.cp_r(fixture_recipe, @tmp_recipes_path)
 end
