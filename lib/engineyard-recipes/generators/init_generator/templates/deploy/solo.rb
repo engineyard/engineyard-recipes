23b400a8a0bd42a931cb4b2f39ec284611ae142d
Dr Nic Williams <drnicwilliams@gmail.com>
2012-03-09 20:47:00 -0800

    chef-on-deploy assumes cookbooks in deploy/cookbooks now

--- lib/engineyard-recipes/generators/init_generator/templates/deploy/solo.rb
+++ lib/engineyard-recipes/generators/init_generator/templates/deploy/solo.rb
@@ -3,5 +3,5 @@ base_dir = File.dirname(__FILE__)
 
 file_store_path   base_dir
 file_cache_path   base_dir
-cookbook_path     "#{base_dir}/../cookbooks"
+cookbook_path     "#{base_dir}/cookbooks"
 node_name         DNApi.from(File.read("/etc/chef/dna.json")).id
